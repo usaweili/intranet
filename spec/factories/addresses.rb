aa58887cd08908ab6ac769d4a3ef50424a092794
Suraj Rajput <suraj21071998@gmail.com>
2019-05-23 14:47:24 +0530

    Used faker in factories, improved code indentation

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,13 +1,10 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :address do
-    flat_or_house_no '1'
-    building_or_society_name 'Test Society'
-    road 'Test Road'
-    locality 'Test locality'
-    city 'Test City'
-    state 'Test State'
-    phone_no '1234567890'
+    address { Faker::Address.full_address }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    landline_no { Faker::PhoneNumber.cell_phone }
+    pin_code { Faker::Address.postcode }
+    company
   end
 end
