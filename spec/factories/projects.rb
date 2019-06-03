ce0bbd3c6f3126a673b1560f2ac114128add7f76
Suraj Rajput <suraj21071998@gmail.com>
2019-06-03 16:46:42 +0530

    Modified factory to increase randomness

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name { Faker::App.name }
     code_climate_id { Faker::Bank.swift_bic }
     code_climate_snippet { Faker::Book.title }
-    code { Faker::Bank.swift_bic }
+    code { Faker::Lorem.characters(10) }
     rails_version { Faker::App.semantic_version }
     ruby_version { Faker::App.semantic_version }
     database { "MongoDB" }
