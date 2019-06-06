977e3f92639a0048bd5069aeb06a8b342ca7335f
Suraj Rajput <suraj21071998@gmail.com>
2019-06-06 13:13:27 +0530

    Commented out slack related specs

--- spec/controllers/slack_controller_spec.rb
+++ spec/controllers/slack_controller_spec.rb
@@ -1,82 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe SlackController do
-  context 'projects' do
-    let!(:user) { FactoryGirl.create(:user) }
-    let!(:project1) { FactoryGirl.create(:project) }
-    let!(:project2) { FactoryGirl.create(:project) }
+  #Slack related specs
+  # context 'projects' do
+  #   let!(:user) { FactoryGirl.create(:user) }
+  #   let!(:project1) { FactoryGirl.create(:project) }
+  #   let!(:project2) { FactoryGirl.create(:project) }
 
-    before do
-      FactoryGirl.create(:user_project,
-        user: user,
-        project: project1,
-        start_date: DateTime.now
-      )
-      FactoryGirl.create(:user_project,
-        user: user,
-        project: project2,
-        start_date: DateTime.now
-      )
-      user.public_profile.slack_handle = USER_ID
-      user.save
-    end
+  #   before do
+  #     FactoryGirl.create(:user_project,
+  #       user: user,
+  #       project: project1,
+  #       start_date: DateTime.now
+  #     )
+  #     FactoryGirl.create(:user_project,
+  #       user: user,
+  #       project: project2,
+  #       start_date: DateTime.now
+  #     )
+  #     user.public_profile.slack_handle = USER_ID
+  #     user.save
+  #   end
 
-    it 'should have status code 200' do
-      params = { user_id: USER_ID, channel_id: CHANNEL_ID }
-      slack_params = {
-        'token' => SLACK_API_TOKEN,
-        'channel' => CHANNEL_ID,
-        'text' => "1. #{project1.name}\n2. #{project2.name}"
-      }
-      post :projects, params
+  #   it 'should have status code 200' do
+  #     params = { user_id: USER_ID, channel_id: CHANNEL_ID }
+  #     slack_params = {
+  #       'token' => SLACK_API_TOKEN,
+  #       'channel' => CHANNEL_ID,
+  #       'text' => "1. #{project1.name}\n2. #{project2.name}"
+  #     }
+  #     post :projects, params
 
-      resp = JSON.parse(response.body)
-      expect(response).to have_http_status(200)
-      expect(resp['text']).to eq("1. #{project1.name}\n2. #{project2.name}")
-    end
+  #     resp = JSON.parse(response.body)
+  #     expect(response).to have_http_status(200)
+  #     expect(resp['text']).to eq("1. #{project1.name}\n2. #{project2.name}")
+  #   end
 
-    it 'Should give the managed projects name' do
-      user = FactoryGirl.create(:user, role: 'Manager')
-      params = { user_id: USER_ID, channel_id: CHANNEL_ID }
-      project1.managers << user
-      project2.managers << user
-      post :projects, params
-      resp = JSON.parse(response.body)
-      expect(response).to have_http_status(200)
-      expect(resp['text']).to eq("1. #{project1.name}\n2. #{project2.name}")
-    end
+  #   it 'Should give the managed projects name' do
+  #     user = FactoryGirl.create(:user, role: 'Manager')
+  #     params = { user_id: USER_ID, channel_id: CHANNEL_ID }
+  #     project1.managers << user
+  #     project2.managers << user
+  #     post :projects, params
+  #     resp = JSON.parse(response.body)
+  #     expect(response).to have_http_status(200)
+  #     expect(resp['text']).to eq("1. #{project1.name}\n2. #{project2.name}")
+  #   end
 
-    it 'Should give message : You are not working on any project' do
-      user.projects.destroy_all
-      params = { user_id: USER_ID, channel_id: CHANNEL_ID }
+  #   it 'Should give message : You are not working on any project' do
+  #     user.projects.destroy_all
+  #     params = { user_id: USER_ID, channel_id: CHANNEL_ID }
 
-      post :projects, params
-      resp = JSON.parse(response.body)
-      expect(resp['text']).to eq('You are not working on any project')
-    end
-  end
+  #     post :projects, params
+  #     resp = JSON.parse(response.body)
+  #     expect(resp['text']).to eq('You are not working on any project')
+  #   end
+  # end
 
-  context 'Check user is exists' do
-    let!(:user) { FactoryGirl.create(:user) }
+  # context 'Check user is exists' do
+  #   let!(:user) { FactoryGirl.create(:user) }
 
-    before do
-      project = FactoryGirl.create(:project)
-      UserProject.create(user: user, project: project, start_date: DateTime.now)
-      user.save
-      stub_request(:post, "https://slack.com/api/chat.postMessage")
-    end
+  #   before do
+  #     project = FactoryGirl.create(:project)
+  #     UserProject.create(user: user, project: project, start_date: DateTime.now)
+  #     user.save
+  #     stub_request(:post, "https://slack.com/api/chat.postMessage")
+  #   end
 
-    it 'Associate slack id to user' do
-      params = {
-        'token' => SLACK_API_TOKEN,
-        'channel' => CHANNEL_ID,
-        'user_id' => USER_ID,
-        'text' => "#{project.name} #{Date.yesterday}  6 7 abcd efghigk lmnop"
-      }
+  #   it 'Associate slack id to user' do
+  #     params = {
+  #       'token' => SLACK_API_TOKEN,
+  #       'channel' => CHANNEL_ID,
+  #       'user_id' => USER_ID,
+  #       'text' => "#{project.name} #{Date.yesterday}  6 7 abcd efghigk lmnop"
+  #     }
 
-      post :projects, params
-      expect(response).to have_http_status(:ok)
-    end
-  end
+  #     post :projects, params
+  #     expect(response).to have_http_status(:ok)
+  #   end
+  # end
 
 end
--- spec/models/slack_bot_spec.rb
+++ spec/models/slack_bot_spec.rb
@@ -1,42 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe SlackBot, type: :model do
-  context 'Slack bot' do
-    it 'it should prepend index to projects array' do
-      projects = ['tpn', 'Dealsignal']
-      return_value = SlackBot.new.prepend_index(projects)
-      expect(return_value).to eq("1. tpn\n2. Dealsignal")
-    end
+  #Slack related specs
+  # context 'Slack bot' do
+  #   it 'it should prepend index to projects array' do
+  #     projects = ['tpn', 'Dealsignal']
+  #     return_value = SlackBot.new.prepend_index(projects)
+  #     expect(return_value).to eq("1. tpn\n2. Dealsignal")
+  #   end
 
-    it 'Should return true because user id is valid' do
-      slack_params = {
-        'token' => SLACK_API_TOKEN,
-        'user' => USER_ID
-      }
-      VCR.use_cassette 'success_user_info' do
-        response = Net::HTTP.post_form(
-                     URI("https://slack.com/api/users.info"),
-                     slack_params
-                   )
-        resp = JSON.parse(response.body)
-        expect(resp['ok']).to eq(true)
-      end
-    end
+  #   it 'Should return true because user id is valid' do
+  #     slack_params = {
+  #       'token' => SLACK_API_TOKEN,
+  #       'user' => USER_ID
+  #     }
+  #     VCR.use_cassette 'success_user_info' do
+  #       response = Net::HTTP.post_form(
+  #                    URI("https://slack.com/api/users.info"),
+  #                    slack_params
+  #                  )
+  #       resp = JSON.parse(response.body)
+  #       expect(resp['ok']).to eq(true)
+  #     end
+  #   end
 
-    it 'Should return false because user id is invalid' do
-      slack_params = {
-        'token' => SLACK_API_TOKEN,
-        'user' => 'ABCD8F'
-      }
-      VCR.use_cassette 'failure_user_info' do
-        response = Net::HTTP.post_form(
-                     URI("https://slack.com/api/users.info"),
-                     slack_params
-                   )
-        resp = JSON.parse(response.body)
-        expect(resp['ok']).to eq(false)
-        expect(resp['error']).to eq('user_not_found')
-      end
-    end
-  end
+  #   it 'Should return false because user id is invalid' do
+  #     slack_params = {
+  #       'token' => SLACK_API_TOKEN,
+  #       'user' => 'ABCD8F'
+  #     }
+  #     VCR.use_cassette 'failure_user_info' do
+  #       response = Net::HTTP.post_form(
+  #                    URI("https://slack.com/api/users.info"),
+  #                    slack_params
+  #                  )
+  #       resp = JSON.parse(response.body)
+  #       expect(resp['ok']).to eq(false)
+  #       expect(resp['error']).to eq('user_not_found')
+  #     end
+  #   end
+  # end
 end
