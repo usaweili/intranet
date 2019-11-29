07789acbf6a16f774123418eb7182cf2c04c230e
Swapnil Chincholkar <swapnil@joshsoftware.com>
2019-11-29 13:29:19 +0530

    Updated code for session store in dev env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-#Intranet::Application.config.session_store :cookie_store, key: '_intranet_session'
 if Rails.env.production?
   Intranet::Application.config.session_store :redis_store, key: '_intranet_session', serializer: :json, redis: {key: 'intranet:session:', url: ENV['REDIS_SESSIONS_URL']}
 elsif Rails.env.staging?
   Intranet::Application.config.session_store :redis_store, key: '_stage_intranet_session', serializer: :json, redis: {key: 'stage-intranet:session:', url: ENV['REDIS_SESSIONS_URL']}
 else
-  Intranet::Application.config.session_store :cookie_store, key: '_intranet_session'
+  Intranet::Application.config.session_store :redis_store, key: '_dev_intranet_session', serializer: :json, redis: {key: 'dev-intranet:session:', url: ENV['REDIS_SESSIONS_URL']}
 end
