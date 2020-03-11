c7dcebbc080399532aa65611dfbc30dd2e7d8700
Swapnil Chincholkar <swapnil@joshsoftware.com>
2020-03-11 20:56:00 +0530

    Added mail interceptor on staging also

--- config/initializers/smtp_config.rb
+++ config/initializers/smtp_config.rb
@@ -1,1 +1,1 @@
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development? || Rails.env.staging?
