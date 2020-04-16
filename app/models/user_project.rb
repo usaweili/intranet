41d17d697f5761d9fbe8d16cc1e1b88de3f44f50
Rohit Joshi <rohit.joshi@joshsoftware.com>
2020-04-17 00:42:12 +0530

    Add controller spec for active_or_all_filter
    Add columns active, time_sheet, and allocation
    Add view to update team details
    Add allocation field to user_projects
    Add allocation field in edit team members view
    Disable inactive team member row
    Add validation for end_date if user is inactive
    Remove actions column from Project show page.
    Remove button to add team members.
    Add column role to display managers.
    Allow free input to allocation and validate its presence
    Change user_project validations
    Write RSpec tests for user_project Model
    Write RSpec tests for user_project Controller
    UserProject list now in ascending order
    Use select2 instead of select
    Query now in controller instead of view
    Modify load_users method
    fixed issues in team forms
    
    + updated code for better errors & view
    + added ex employee details
    + fixed issue in project user form
    + Remove Add team member option in edit project
    + Add manager to user_project when added to project
    + Add scope to select active and inactive users
    + Display inactive and ex employees both
    + Switch to team detail tab on error
    + Add validation for unique project users
    + Add model test case for validation of unique user id for active users
    + fixed old employees stats for timesheet requred flag

--- app/models/user_project.rb
+++ app/models/user_project.rb
@@ -1,12 +1,35 @@
 class UserProject
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   field :start_date, type: Date
   field :end_date, type: Date, default: nil
-  
+  field :time_sheet, type: Boolean, default: false
+  field :active, type:Boolean, default: true
+  field :allocation, type: Integer, default: 100
+
   belongs_to :user
   belongs_to :project
-  
-  validates :start_date, :user_id, :project_id, presence: true
+
+  validates :user_id, :project_id, :start_date, :active, :allocation, presence: true
+  validates :user_id, uniqueness: {scope: :project_id}, if: :active_user?
+  validates :allocation, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 1, less_than_or_equal_to: 100,
+      message: 'should be between range of 1-100'
+  }
+
+
+  validates :end_date, presence: {unless: "!!active || active.nil?", message: "is mandatory to mark inactive"}
+
+  scope :approved_users, ->{where(:user_id.in => User.approved.pluck(:id))}
+  scope :active_users, ->{where(:user_id.in => User.approved.pluck(:id), :active => true)}
+  scope :inactive_users, ->{where(:user_id.in => User.approved.pluck(:id), :active => false)}
+  scope :ex_users, ->{where(:user_id.nin => User.approved.pluck(:id))}
+end
+
+def active_user?
+  UserProject.where(project_id: project_id, user_id: user_id).pluck(:active).inject do |final_user_active, current_user_active|
+     final_user_active || current_user_active
+  end
 end
