f1b44228beefe0de10b05bbf7d8d1ea0b419f61b
Abhishek Sapate <sapate.abhishek1998@gmail.com>
2020-04-20 15:09:26 +0530

    fixed testcases for timesheet

--- spec/factories/time_sheet.rb
+++ spec/factories/time_sheet.rb
@@ -6,5 +6,9 @@ FactoryGirl.define do
     description { Faker::Lorem.sentences(2) }
     user
     project
+    after(:build) do |obj|
+      obj.from_time = "#{obj.date} 10:00" if obj.from_time == nil
+      obj.to_time = "#{obj.date} 11:00" if obj.to_time == nil
+    end
   end
 end
