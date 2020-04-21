bc5249dfb319ce4b44f0ee0ded809d9e7389d450
Abhishek Sapate <sapate.abhishek1998@gmail.com>
2020-04-22 01:16:12 +0530

    bug fixed for timesheet spec

--- spec/factories/time_sheet.rb
+++ spec/factories/time_sheet.rb
@@ -1,8 +1,6 @@
 FactoryGirl.define do
   factory :time_sheet do
     date { Date.today - 1 }
-    from_time { Time.parse('10:00') }
-    to_time { Time.parse('11:00') }
     description { Faker::Lorem.sentences(2) }
     user
     project
