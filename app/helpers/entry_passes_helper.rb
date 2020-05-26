0a51711deb5cb52594f30a3457d6e431eb9741d8
Swapnil Chincholkar <swapnil@joshsoftware.com>
2020-05-26 21:24:43 +0530

    Fixed code for many bugs & enhancements

--- app/helpers/entry_passes_helper.rb
+++ app/helpers/entry_passes_helper.rb
@@ -1,2 +1,14 @@
 module EntryPassesHelper
+
+  def any_employee_registered?(date)
+    @office_passes.select{|pass| pass.date == date}.count > 0
+  end
+
+  def registered_employee_count_for(date)
+    @office_passes.select{|pass| pass.date == date}.count
+  end
+
+  def sorted_list(names_with_id)
+    names_with_id.sort_by{|i| i.first.strip.downcase}
+  end
 end
--- app/models/entry_pass.rb
+++ app/models/entry_pass.rb
@@ -3,22 +3,23 @@ class EntryPass
   field :date, type: Date
   belongs_to :user
 
-  validates :date, presence: true, uniqueness: {scope: :user_id, message:": You already have an entry pass for this date"}
+  validates :date, presence: true, uniqueness: {scope: :user_id, message: "already selected"}
   validate :validate_daily_limit
 
-  def self.to_csv
+  def self.to_csv(records)
     attributes = %w{Date Name Email}
     CSV.generate(headers: true) do |csv|
       csv << attributes
-      all.each do |entry_pass|
-        csv << [entry_pass.date, User.where({id: entry_pass.user_id}).first.try(:name), User.where({id: entry_pass.user_id}).first.try(:email)]
+      records.each do |entry_pass|
+        user = entry_pass.user
+        csv << [entry_pass.date, user.try(:name), user.try(:email)]
       end
     end
   end
 
   def validate_daily_limit
     if EntryPass.where({date: self.date}).count >= DAILY_OFFICE_ENTRY_LIMIT
-      errors.add(:date, "Maximum number of employees allowed to work from office reached")
+      errors.add(:date, "Maximum number of employees allowed to work from office is reached")
     end
   end
 end
