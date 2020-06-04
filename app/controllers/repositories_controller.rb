f22ca27e3ebe40bb564ce762ee524a45068614ea
Kaiwalya <kaiwalya.pataskar@joshsoftware.com>
2020-06-04 13:50:12 +0530

    Handled configurable limit in get_issues

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,9 +2,9 @@ class RepositoriesController < ApplicationController
   before_action :authenticate_user!
 
   def overview_index
-    url = "http://api.codeclimate.com/v1/orgs/#{ENV["CODE_CLIMATE_ORGANIZATION_KEY"]}/repos"
-    headers = { "Accept" => "application/vnd.api+json",
-                "Authorization" => "Token token=#{ENV["CODE_CLIMATE_TOKEN"]}" }
+    url = "http://api.codeclimate.com/v1/orgs/#{ENV['CODE_CLIMATE_ORGANIZATION_KEY']}/repos"
+    headers = { 'Accept' => 'application/vnd.api+json',
+                'Authorization' => "Token token=#{ENV['CODE_CLIMATE_TOKEN']}" }
     begin
       response = HTTParty.get(url, headers: headers, timeout: 20)
     rescue Timeout::Error => e
@@ -19,28 +19,29 @@ class RepositoriesController < ApplicationController
   end
 
   def get_repo_issues
-    offset = params["offset"].to_i || 0
-    page = params["page"] || 1
-    result = get_issues(page = page)
+    offset = params['offset'].to_i || 0
+    page = params['page']
+    limit = params['limit']
+    result = get_issues(page = page, limit)
     respond_to do |format|
-      format.json { render json: result["data"] || {} }
+      format.json { render json: result['data'] || {} }
     end
   end
 
   private
 
   # Recursive get_issues to fetch the paginated data from CodeClimate, where page[size] limit is 100.
-  def get_issues(page = 1)
-    query_string = "page[size]=100&page[number]=#{page}"
+  def get_issues(page = 1, limit = 100)
+    query_string = "page[size]=#{limit}&page[number]=#{page}"
     url = "https://api.codeclimate.com/v1/repos/#{params[:repo_id]}/snapshots/#{params[:snap_id]}/issues?#{query_string}"
-    headers = { "Accept" => "application/vnd.api+json", "Authorization" => "Token token=#{ENV["CODE_CLIMATE_TOKEN"]}" }
+    headers = { 'Accept' => 'application/vnd.api+json', 'Authorization' => "Token token=#{ENV['CODE_CLIMATE_TOKEN']}" }
     begin
       response = HTTParty.get(url, headers: headers, timeout: 20)
     rescue Timeout::Error => e
       puts "Error: Request Timeout for #{repo.project.name}"
     end
     response = JSON.parse(response.body)
-    if response["meta"]["current_page"] <= response["meta"]["total_pages"]
+    if response && response['meta'] && response['meta']['current_page'] <= response['meta']['total_pages']
       return response
     else
       return {}
