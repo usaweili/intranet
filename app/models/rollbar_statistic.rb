42f9fe8519df9318e87ca5918e93c3b624130c40
Kaiwalya <kaiwalya.pataskar@joshsoftware.com>
2020-07-10 11:11:40 +0530

    Removed default: 0 from rollbar_statistics

--- app/models/rollbar_statistic.rb
+++ app/models/rollbar_statistic.rb
@@ -3,9 +3,9 @@ class RollbarStatistic
   include Mongoid::Timestamps
   belongs_to :repository
   field :date
-  field :total_issues, type: Integer, default: 0
-  field :active_issue_count, type: Integer, default: 0
-  field :resolved_issue_count, type: Integer, default: 0
-  field :new_issue_count, type: Integer, default: 0 # Occured for the first time in the given week.
+  field :total_issues, type: Integer
+  field :active_issue_count, type: Integer
+  field :resolved_issue_count, type: Integer
+  field :new_issue_count, type: Integer # Occured for the first time in the given week.
   validates_presence_of :date, :repository
 end
