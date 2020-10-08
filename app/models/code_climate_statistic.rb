463f6e6709ea8b0279c4ff801132e9fea359ef44
Suraj Rajput <suraj21071998@gmail.com>
2020-10-08 17:52:15 +0530

    Updated logic to fetch the code_climate_statistics

--- app/models/code_climate_statistic.rb
+++ app/models/code_climate_statistic.rb
@@ -1,15 +1,17 @@
 class CodeClimateStatistic
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :timestamp
-  field :gpa # Grade Point Average
-  field :test_coverage
-  field :loc, default: {} # Language-wise LOC
-  field :maintainability
-  field :remediation_minutes
-  field :technical_debt_ratio
-  field :diff_coverage
-  field :ratings, default: {} # File types from A to F 
+
+  field :lines_of_code,        type: Integer, default: 0
+  field :total_issues,         type: Integer, default: 0
+  field :complexity_issues,    type: Integer, default: 0
+  field :duplication_issues,   type: Integer, default: 0
+  field :maintainability,      type: Float,   default: 0.0
+  field :quality_gpa,          type: Float,   default: 0.0
+  field :remediation_time,     type: Float,   default: 0.0
+  field :implementation_time,  type: Float,   default: 0.0
+  field :test_coverage,        type: Float,   default: 0.0
+  field :technical_debt_ratio, type: Float,   default: 0.0
+
   belongs_to :repository
-  validates_presence_of :timestamp, :repository
 end
