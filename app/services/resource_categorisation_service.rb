b9ffdb810a85fc026c13117bf9899b9a3b17b91a
Tejas Zambre <tzambre681@gmail.com>
2020-10-28 18:08:10 +0530

    Resource Categorisation changes
    - Added location field to billable resource report(first sheet)
    - Added Project wise resource categorisation report as new sheet in resource categorisation report xls for project wise details of employees
    	1. New sheet have same fields like resource categorisation except allocation and bench
    	2. Employee designation field is added to project wise resource categorisation(second sheet)
    - Added load method as before filter to load all required resources
    - Updated the test cases related to all above changes
    - Added Technical Skills field in billable resource report(first sheet)

--- app/services/resource_categorisation_service.rb
+++ app/services/resource_categorisation_service.rb
@@ -2,68 +2,126 @@ class ResourceCategorisationService
 
   def initialize(emails)
     @emails = emails
+    load_projects
   end
 
   def call
-    files = { resources: generate_resource_report }
-    ReportMailer.send_resource_categorisation_report(files, @emails).deliver_now
+    generate_resource_report
+    ReportMailer.send_resource_categorisation_report(@report, @emails).deliver_now
   end
 
   def generate_resource_report
-    resource_report = []
     exclude_designations = [
       'Assistant Vice President - Sales',
       'Business Development Executive',
       'Office Assistant'
     ]
-    User.approved.where(:role.in => ['Employee', 'Intern']).each do |user|
+
+    User.approved.where(:role.in => ['Employee', 'Intern'], :'employee_detail.location'.ne => LOCATIONS[0]).each do |user|
       unless exclude_designations.include?(user.designation.try(:name))
-        billable_allocation = billable_projects_allocation(user.id)
+        billable_allocation = billable_projects_allocation(user)
         billable_allocation = billable_allocation > 160 ? 160 : billable_allocation
-        non_billable_allocation = non_billable_projects_allocation(user.id)
-        investment_allocation = investment_projects_allocation(user.id)
+        non_billable_allocation = non_billable_projects_allocation(user)
+        investment_allocation = investment_projects_allocation(user)
         total_allocation = billable_allocation + non_billable_allocation + investment_allocation
         bench_allocation =  (160 - total_allocation) < 0 ? 0 : (160 - total_allocation)
         project_names = user.project_details.map { |i| i.values[1] }
-        resource_report << { name: user.name,
-                             total_allocation: total_allocation,
-                             billable: billable_allocation,
-                             non_billable: non_billable_allocation,
-                             investment: investment_allocation,
-                             bench: bench_allocation,
-                             projects: project_names.join(', ')
-                            }
+
+        @report[:resource_report] << {
+          name: user.name,
+          location: user.location,
+          total_allocation: total_allocation,
+          billable: billable_allocation,
+          non_billable: non_billable_allocation,
+          investment: investment_allocation,
+          bench: bench_allocation,
+          technical_skills: [user.public_profile.try('technical_skills')].join(', '),
+          projects: project_names.join(', ')
+        }
       end
     end
-    resource_report.sort_by { |k,v| k[:name] }
+
+    @report[:resource_report] = @report[:resource_report].sort_by { |k,v| k[:name] }
+    @report[:project_wise_resource_report] = @report[:project_wise_resource_report].sort_by { |k,v| k[:project] }
+    @report
   end
 
-  def billable_projects_allocation(user_id)
-    projects = Project.where(:type_of_project.in => ['T&M', 'Fixbid'], is_active: true)
-    UserProject.where(
-      :project_id.in => projects.pluck(:id),
+  def billable_projects_allocation(user)
+    user_projects = UserProject.where(
+      :project_id.in => @billable_projects,
       active: true,
       billable: true,
-      user_id: user_id
-    ).pluck(:allocation).sum
+      user_id: user.id
+    )
+    
+    add_project_wise_details(user, user_projects, 'billable')
+    user_projects.pluck(:allocation).sum
   end
 
-  def non_billable_projects_allocation(user_id)
-    projects = Project.where(:type_of_project.in => ['T&M', 'Free', 'Fixbid'], is_active: true)
-    UserProject.where(
-      :project_id.in => projects.pluck(:id),
+  def non_billable_projects_allocation(user)
+    user_projects = UserProject.where(
+      :project_id.in => @non_billable_projects,
       active: true,
       billable: false,
-      user_id: user_id
-    ).pluck(:allocation).sum
+      user_id: user.id
+    )
+
+    add_project_wise_details(user, user_projects, 'non_billable')
+    user_projects.pluck(:allocation).sum
   end
 
-  def investment_projects_allocation(user_id)
-    projects = Project.where(type_of_project: 'Investment', is_active: true, is_activity: false)
-    UserProject.where(
-      :project_id.in => projects.pluck(:id),
+  def investment_projects_allocation(user)
+    user_projects = UserProject.where(
+      :project_id.in => @investment_projects,
       active: true,
-      user_id: user_id
-    ).pluck(:allocation).sum
+      user_id: user.id
+    )
+
+    add_project_wise_details(user, user_projects, 'investment')
+    user_projects.pluck(:allocation).sum
+  end
+
+  def add_project_wise_details(user, user_projects, allocation_type)
+    user_projects.each do |user_project|
+      @report[:project_wise_resource_report] << add_record(user).merge(
+        "#{allocation_type}": user_project.allocation,
+        project: user_project.project.name
+      )
+    end
+  end
+
+  def add_record(user)
+    { 
+      name: user.name,
+      location: user.location,
+      designation: user.designation.try(:name),
+      billable: 0,
+      non_billable: 0,
+      investment: 0,
+      project: ''
+    }
+  end
+
+  def load_projects
+    @report = {
+      resource_report: [],
+      project_wise_resource_report: []
+    }
+
+    @billable_projects = Project.where(
+      :type_of_project.in => ['T&M', 'Fixbid'],
+      is_active: true
+    ).pluck(:id)
+    
+    @non_billable_projects = Project.where(
+      :type_of_project.in => ['T&M', 'Free', 'Fixbid'],
+      is_active: true
+    ).pluck(:id)
+
+    @investment_projects = Project.where(
+      type_of_project: 'Investment',
+      is_active: true,
+      is_activity: false
+    ).pluck(:id)
   end
 end
